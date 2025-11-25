f19a491882826cb4dacd9570525c1e6bf3781dca
Saksham Singh <62788658+sakshamssr@users.noreply.github.com>
2025-11-25 23:28:59 +0530

    Update ALLOWED_HOSTS for deployment
    Added '127.0.0.1', '.vercel.app', and '.now.sh' to ALLOWED_HOSTS to support local development and deployment on Vercel and Now platforms.

--- demostocks/settings.py
+++ demostocks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)^2xuf0e-z@fv9(lq(vqn6@hg_-4&qd+p9plc23s59s)4e96z+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', '.now.sh']
 
 
 # Application definition
